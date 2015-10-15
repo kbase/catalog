e3cb438e5f6fb37d7d4286d37644f4dbe6ade73a
Keith Keller <kkeller@lbl.gov>
2015-10-15 09:47:44 -0700

    Added code to checkout repository (default branch, usually master unless github owner has changed).

--- lib/biokbase/catalog/registrar.py
+++ lib/biokbase/catalog/registrar.py
@@ -3,6 +3,8 @@
 
 import time
 import sys
+import git
+from urlparse import urlparse
 
 from biokbase.catalog.db import MongoCatalogDBI
 from docker import Client as DockerClient
@@ -29,8 +31,12 @@ class Registrar:
         # if image does not exist, build and set state
         # if instance does not exist, start and set state
 	dockerclient = DockerClient(base_url = str(self.docker_base_url))
+        git_url=self.params['git_url']
+        parsed_url=urlparse(git_url)
+        repo = git.Repo.clone_from(git_url,self.temp_dir+parsed_url.path)
         somefile = open(self.temp_dir+'/file'+str(self.timestamp), 'w')
-        somefile.write('doing something\n');
-        somefile.write(str(dockerclient.containers()));
+#        somefile.write('doing something\n');
+#        somefile.write(str(dockerclient.containers()));
+        somefile.write(str(parsed_url.path));
         somefile.write(str(self.params));
         somefile.close();
