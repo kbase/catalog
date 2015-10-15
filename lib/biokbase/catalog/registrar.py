da4d7eceadfbc5808582b8de7f57e51e3ef05572
Keith Keller <kkeller@lbl.gov>
2015-10-15 12:55:49 -0700

    Rearranged and added comments for how I want to do the Docker steps.

--- lib/biokbase/catalog/registrar.py
+++ lib/biokbase/catalog/registrar.py
@@ -1,13 +1,11 @@
-
-
-
 import time
 import sys
 import git
+import requests
 from urlparse import urlparse
+from docker import Client as DockerClient
 
 from biokbase.catalog.db import MongoCatalogDBI
-from docker import Client as DockerClient
 
 
 class Registrar:
@@ -25,15 +23,20 @@ class Registrar:
         self.docker_base_url = docker_base_url
 
     def start_registration(self):
-        # pseudocode
-        # get_repo_details
-        # look for docker image / instance
-        # if image does not exist, build and set state
-        # if instance does not exist, start and set state
-	dockerclient = DockerClient(base_url = str(self.docker_base_url))
+
         git_url=self.params['git_url']
         parsed_url=urlparse(git_url)
+        # really should check whether dir exists already, and
+        # whether it would be appropriate to remove and re-clone
+        # or simply pull
         repo = git.Repo.clone_from(git_url,self.temp_dir+parsed_url.path)
+
+	dockerclient = DockerClient(base_url = str(self.docker_base_url))
+        # look for docker image / instance (might need an http call,
+        # dockerhub-ci does not seem to respond to search API)
+        # if image does not exist, build, push to dockerhub-ci, and set state
+        # if instance does not exist, start and set state
+
         somefile = open(self.temp_dir+'/file'+str(self.timestamp), 'w')
 #        somefile.write('doing something\n');
 #        somefile.write(str(dockerclient.containers()));
