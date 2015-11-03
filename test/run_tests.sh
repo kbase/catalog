1d438471bac861fd6b4bbb35bd35cdb243e6e165
msneddon <mwsneddon@lbl.gov>
2015-11-02 17:34:49 -0800

    updated nms again

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,2 +1,14 @@
+#!/bin/sh
+
+# start the test NMS endpoint
+echo 'Starting NMS...'
+export KB_DEPLOYMENT_CONFIG=test.cfg
+../narrative_method_store/dist/run_test_nms_endpoint.sh 7125 > nms/error.log 2>&1 &
+NMS_PID=$!
+echo 'Waiting for NMS to start...'
+sleep 20
+
 export PYTHONPATH=pylib:$PYTHONPATH
 python -m unittest discover -p "*_test.py"
+
+kill $NMS_PID
