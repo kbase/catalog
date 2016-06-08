a99ebfc9805890197fee193c5ed859ba796c3d63
msneddon <mwsneddon@lbl.gov>
2016-06-08 15:54:18 -0700

    added some volume mount configuration tests

--- test/client_group_test.py
+++ test/client_group_test.py
@@ -140,13 +140,168 @@ class ClientGroupMethodsTest(unittest.TestCase):
         userCtx = self.cUtil.user_ctx()
         adminCtx = self.cUtil.admin_ctx()
 
-        filter = {}
-        vol_mounts = self.catalog.list_volume_mounts(adminCtx, filter)[0]
-        pprint(vol_mounts)
+        # mere users cannot list, set, or remove mounts
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(userCtx, {})
+        self.assertEqual(str(e.exception),
+            'You do not have permission to set volume mounts.');
 
+        with self.assertRaises(ValueError) as e:
+            self.catalog.remove_volume_mount(userCtx, {})
+        self.assertEqual(str(e.exception),
+            'You do not have permission to remove volume mounts.');
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.list_volume_mounts(userCtx, {})
+        self.assertEqual(str(e.exception),
+            'You do not have permission to view volume mounts.');
+
+        # basic filter should return the preloaded mounts, of which there are 4
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, {})[0]
+        self.assertEqual(len(vol_mounts),4)
+
+        # try some other filters, make sure we get the right number of things
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, { 'module_name':'VolmountModTest1' })[0]
+        self.assertEqual(len(vol_mounts),1)
+        self.assertEqual(vol_mounts[0]['module_name'],'VolMountModTest1')
+        self.assertEqual(vol_mounts[0]['app_id'],'func1')
+        self.assertEqual(vol_mounts[0]['client_group'],'G1')
+        self.assertEqual(len(vol_mounts[0]['volume_mounts']), 2)
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['host_dir'], '/home/wstester1')
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['container_dir'], '/mnt/tmp')
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['read_only'], 1)
+
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, { 'module_name':'VolMountModTest2' })[0]
+        self.assertEqual(len(vol_mounts),3)
+
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, { 'app_id':'func1' })[0]
+        self.assertEqual(len(vol_mounts),3)
+
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, { 'client_group':'G3' })[0]
+        self.assertEqual(len(vol_mounts),2)
+
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, { 'module_name':'VolMountModTest2', 'app_id':'func1' })[0]
+        self.assertEqual(len(vol_mounts),2)
+
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, { 'module_name':'VolMountModTest2', 'app_id':'func1', 'client_group':'G2' })[0]
+        self.assertEqual(len(vol_mounts),1)
+        self.assertEqual(vol_mounts[0]['module_name'],'VolMountModTest2')
+        self.assertEqual(vol_mounts[0]['app_id'],'func1')
+        self.assertEqual(vol_mounts[0]['client_group'],'G2')
+        self.assertEqual(len(vol_mounts[0]['volume_mounts']), 1)
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['host_dir'], '/home/wstester1')
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['container_dir'], '/mnt/tmp2')
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['read_only'], 0)
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.list_volume_mounts(adminCtx, { 'module_name': [] })
+        self.assertEqual(str(e.exception),
+            'module_name parameter field must be a string');
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.list_volume_mounts(adminCtx, { 'app_id': [] })
+        self.assertEqual(str(e.exception),
+            'app_id parameter field must be a string');
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.list_volume_mounts(adminCtx, { 'client_group': [] })
+        self.assertEqual(str(e.exception),
+            'client_group parameter field must be a string');
+
+
+
+        # try to add a volume mount, keep adding things till we get a success
+        volume_mount_config = {}
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'module_name parameter field is required');
+
+        volume_mount_config['module_name'] = []
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'module_name parameter field must be a string');
+        volume_mount_config['module_name'] = 'Tester2'
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'app_id parameter field is required');
+
+        volume_mount_config['app_id'] = []
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'app_id parameter field must be a string');
+        volume_mount_config['app_id'] = 'my_app'
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'client_group parameter field is required');
+
+        volume_mount_config['client_group'] = []
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'client_group parameter field must be a string');
+        volume_mount_config['client_group'] = 'g23123'
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'volume_mounts parameter field is required');
+
+        volume_mount_config['volume_mounts'] = 'hello'
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'volume_mounts parameter field must be a list');
+        volume_mount_config['volume_mounts'] = [{
+                'host_dir' : '/tmp',
+                'container_dir' : '/tmp/asdf',
+                'read_only': 1
+            }, {}]
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'host_dir parameter field is required in all volume_mount configurations');
+
+        volume_mount_config['volume_mounts'] = [{
+                'host_dir' : '/tmp',
+                'read_only': 1
+            }]
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'container_dir parameter field is required in all volume_mount configurations');
+
+        volume_mount_config['volume_mounts'] = [{
+                'host_dir' : '/tmp',
+                'container_dir' : '/tmp/asdf'
+            }]
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'read_only parameter field is required in all volume_mount configurations');
+
+        volume_mount_config['volume_mounts'] = [{
+                'host_dir' : '/tmp',
+                'container_dir' : '/tmp/asdf',
+                'read_only':2
+            }]
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.set_volume_mount(adminCtx, volume_mount_config)
+        self.assertEqual(str(e.exception),
+            'read_only parameter field in volume_mount list must be either 1 (true) or 0 (false)');
 
         volume_mount_config = {
-            'module_name': 'Tester2',
+            'module_name': 'tester2',
             'app_id':'my_app',
             'client_group':'g23123',
             'volume_mounts' : [
@@ -157,20 +312,70 @@ class ClientGroupMethodsTest(unittest.TestCase):
             }
             ]
         }
+        self.catalog.set_volume_mount(adminCtx, volume_mount_config)
 
+
+        filter = { 'module_name' : 'tester2' }
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, filter)[0]
+        self.assertEqual(len(vol_mounts),1)
+        self.assertEqual(vol_mounts[0]['module_name'],'tester2')
+        self.assertEqual(vol_mounts[0]['app_id'],'my_app')
+        self.assertEqual(vol_mounts[0]['client_group'],'g23123')
+        self.assertEqual(len(vol_mounts[0]['volume_mounts']), 1)
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['host_dir'], '/tmp')
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['container_dir'], '/tmp/asdf')
+        self.assertEqual(vol_mounts[0]['volume_mounts'][0]['read_only'], 1)
+
+
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, {})[0]
+        self.assertEqual(len(vol_mounts),5)
+
+        volume_mount_config = {
+            'module_name': 'tester2',
+            'app_id':'my_app',
+            'client_group':'g23123',
+            'volume_mounts' : []
+        }
         self.catalog.set_volume_mount(adminCtx, volume_mount_config)
 
         filter = { 'module_name' : 'tester2' }
         vol_mounts = self.catalog.list_volume_mounts(adminCtx, filter)[0]
-        pprint(vol_mounts)
+        self.assertEqual(len(vol_mounts),1)
+        self.assertEqual(vol_mounts[0]['module_name'],'tester2')
+        self.assertEqual(vol_mounts[0]['app_id'],'my_app')
+        self.assertEqual(vol_mounts[0]['client_group'],'g23123')
+        self.assertEqual(len(vol_mounts[0]['volume_mounts']), 0)
 
-        del(volume_mount_config['volume_mounts'])
-        self.catalog.remove_volume_mount(adminCtx, volume_mount_config)
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, {})[0]
+        self.assertEqual(len(vol_mounts),5)
 
-        
-        filter = { 'module_name' : 'tester2' }
-        vol_mounts = self.catalog.list_volume_mounts(adminCtx, filter)[0]
-        pprint(vol_mounts)
+
+        # Finally test removal
+        with self.assertRaises(ValueError) as e:
+            self.catalog.remove_volume_mount(adminCtx, {})
+        self.assertEqual(str(e.exception),
+            'module_name parameter field is required');
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, {})[0]
+        self.assertEqual(len(vol_mounts),5)
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.remove_volume_mount(adminCtx, {'module_name':'Tester2'})
+        self.assertEqual(str(e.exception),
+            'app_id parameter field is required');
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, {})[0]
+        self.assertEqual(len(vol_mounts),5)
+
+        with self.assertRaises(ValueError) as e:
+            self.catalog.remove_volume_mount(adminCtx, {'module_name':'Tester2', 'app_id':'my_app'})
+        self.assertEqual(str(e.exception),
+            'client_group parameter field is required');
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, {})[0]
+        self.assertEqual(len(vol_mounts),5)
+
+        self.catalog.remove_volume_mount(adminCtx, {'module_name':'Tester2', 'app_id':'my_app', 'client_group':'g23123'})
+
+        vol_mounts = self.catalog.list_volume_mounts(adminCtx, {})[0]
+        self.assertEqual(len(vol_mounts),4)
 
         return
 
